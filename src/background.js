baa60a088f7732c435d42fb309c8c76b2132cd87
TheGreatFoxxy <56609591+TheGreatFoxxy@users.noreply.github.com>
2020-10-08 16:15:52 -0500

    set up Context Menu

--- src/background.js
+++ src/background.js
@@ -1,6 +1,14 @@
   chrome.browserAction.setBadgeText({text: 'ON'});
   chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
   chrome.runtime.onInstalled.addListener(function() {
+        for (let key of Object.keys(kLocales)) {
+      chrome.contextMenus.create({
+        id: key,
+        title: kLocales[key],
+        type: 'normal',
+        contexts: ['selection'],
+      });
+    };
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([
         {
